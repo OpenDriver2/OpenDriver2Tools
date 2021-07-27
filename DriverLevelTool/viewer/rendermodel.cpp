c5606a96d7f2822195d862a3499e2404210d70cd
Ilya Shurumov <ishurumov@gmail.com>
2021-07-27 12:11:36 +0600

    - fix Driver 1 polygon types

--- DriverLevelTool/viewer/rendermodel.cpp
+++ DriverLevelTool/viewer/rendermodel.cpp
@@ -164,9 +164,9 @@ genBatch_t* FindBatch(Array<genBatch_t*>& batches, int tpageId)
 void CRenderModel::GenerateBuffers()
 {
 	Array<genBatch_t*>		batches;
-	Array<GrVertex>		vertices;
+	Array<GrVertex>			vertices;
 	Array<vertexTuple_t>	verticesMap;
-	
+
 	MODEL* model = m_sourceModel->model;
 	MODEL* vertex_ref = model;
 
@@ -176,7 +176,7 @@ void CRenderModel::GenerateBuffers()
 	}
 
 	genBatch_t* batch = nullptr;
-	
+
 	int modelSize = m_sourceModel->size;
 	int face_ofs = 0;
 	dpoly_t dec_face;
@@ -195,7 +195,13 @@ void CRenderModel::GenerateBuffers()
 			break;
 		}
 
-		int poly_size = decode_poly(facedata, &dec_face);
+		int forcePolyType = -1;
+
+		// [A] HACK: is sky? force POLYFT4. This fixes VEGAS skies
+		if (m_sourceModel->index < 4)
+			forcePolyType = 21;
+
+		int poly_size = decode_poly(facedata, &dec_face, forcePolyType);
 
 		// check poly size
 		if (poly_size == 0)
@@ -208,6 +214,7 @@ void CRenderModel::GenerateBuffers()
 
 		int numPolyVerts = (dec_face.flags & FACE_IS_QUAD) ? 4 : 3;
 		bool bad_face = false;
+		bool bad_normals = false;
 
 		// perform vertex checks
 		for (int v = 0; v < numPolyVerts; v++)
@@ -223,7 +230,7 @@ void CRenderModel::GenerateBuffers()
 			{
 				if (dec_face.nindices[v] >= vertex_ref->num_point_normals)
 				{
-					bad_face = true;
+					bad_normals = true;
 					break;
 				}
 			}
@@ -232,7 +239,7 @@ void CRenderModel::GenerateBuffers()
 		if (bad_face)
 		{
 			MsgError("poly id=%d type=%d ofs=%d has invalid indices (or format is unknown)\n", i, *facedata & 31, model->poly_block + face_ofs);
-			
+
 			continue;
 		}
 
@@ -241,18 +248,18 @@ void CRenderModel::GenerateBuffers()
 
 		if (tpageId == 255)
 			tpageId = -1;
-		
-		if(batch && batch->tpage != tpageId)
+
+		if (batch && batch->tpage != tpageId)
 			batch = FindBatch(batches, tpageId);
-		
+
 		if (!batch)
 		{
 			batch = new genBatch_t;
 			batch->tpage = tpageId;
-	
+
 			batches.append(batch);
 		}
-		
+
 		// Gouraud-shaded poly smoothing
 		bool smooth = (dec_face.flags & FACE_VERT_NORMAL);
 
@@ -265,16 +272,19 @@ void CRenderModel::GenerateBuffers()
 #define VERT_IDX v//numPolyVerts - 1 - v
 
 			int vflags = dec_face.flags & ~(FACE_IS_QUAD | FACE_RGB);
-			
+
 			// try searching for vertex
 			int index = FindGrVertexIndex(verticesMap,
 				vflags,
-				dec_face.vindices[VERT_IDX], 
-				dec_face.nindices[VERT_IDX], 
+				dec_face.vindices[VERT_IDX],
+				dec_face.nindices[VERT_IDX],
 				*(ushort*)dec_face.uv[VERT_IDX]);
 
+			//if (vertexModCb)
+			//	index = -1;
+
 			// add new vertex
-			if(index == -1)
+			if (index == -1)
 			{
 				GrVertex newVert;
 				vertexTuple_t vertMap;
@@ -283,33 +293,45 @@ void CRenderModel::GenerateBuffers()
 				vertMap.normalIndex = -1;
 				vertMap.vertexIndex = dec_face.vindices[VERT_IDX];
 				vertMap.uvs = *(ushort*)dec_face.uv[VERT_IDX];
-				
+
 				// get the vertex
 				SVECTOR* vert = vertex_ref->pVertex(dec_face.vindices[VERT_IDX]);
 				Vector3D fVert = Vector3D(vert->x * RENDER_SCALING, vert->y * -RENDER_SCALING, vert->z * RENDER_SCALING);
-				
+
 				(*(Vector3D*)&newVert.vx) = fVert;
 
+				// set color
+				newVert.cr = newVert.cg = newVert.cb = newVert.ca = 1.0f;
+
+				// add bounding box stuff
 				AddExtentVertex(m_extMin, m_extMax, fVert);
 
-				if (smooth)
+				if (smooth && !bad_normals)
 				{
 					vertMap.normalIndex = dec_face.nindices[VERT_IDX];
-					
+
 					SVECTOR* norm = vertex_ref->pPointNormal(vertMap.normalIndex);
-					*(Vector3D*)&newVert.nx = Vector3D(norm->x * RENDER_SCALING, norm->y * -RENDER_SCALING, norm->z * RENDER_SCALING);
+					*(Vector3D*)&newVert.nx = -Vector3D(norm->x * RENDER_SCALING, norm->y * -RENDER_SCALING, norm->z * RENDER_SCALING);
 				}
-				
+
 				if (dec_face.flags & FACE_TEXTURED)
 				{
 					UV_INFO uv = *(UV_INFO*)dec_face.uv[VERT_IDX];
 
 					// map to 0..1
-					newVert.tc_u = ((float)uv.u + 0.5f) / 256.0f;
-					newVert.tc_v = ((float)uv.v + 0.5f) / 256.0f;
+					newVert.tc_u = ((float)uv.u + 0.5f) / TEXPAGE_SIZE_Y;
+					newVert.tc_v = ((float)uv.v + 0.5f) / TEXPAGE_SIZE_Y;
+				}
+
+				if (dec_face.flags & FACE_RGB)
+				{
+					newVert.cr = dec_face.color.r / 255;
+					newVert.cg = dec_face.color.g / 255;
+					newVert.cb = dec_face.color.b / 255;
 				}
 
 				index = vertMap.grVertexIndex = vertices.size();
+
 				vertices.append(newVert);
 
 				// add vertex and a map
@@ -318,20 +340,20 @@ void CRenderModel::GenerateBuffers()
 				// vertices and verticesMap should be equal
 				assert(verticesMap.size() == vertices.size());
 			}
-			
+
 			// add index
 			faceIndices[v] = index;
 		}
 
 		// if not gouraud shaded we just compute face normal
 		// FIXME: make it like game does?
-		if(!smooth)
+		if (!smooth || bad_normals)
 		{
 			// it takes only triangle
 			Vector3D v0 = *(Vector3D*)&vertices[faceIndices[0]].vx;
 			Vector3D v1 = *(Vector3D*)&vertices[faceIndices[1]].vx;
 			Vector3D v2 = *(Vector3D*)&vertices[faceIndices[2]].vx;
-			
+
 			Vector3D normal = normalize(cross(v2 - v1, v0 - v1));
 
 			// set to each vertex
@@ -340,7 +362,7 @@ void CRenderModel::GenerateBuffers()
 		}
 
 		// triangulate quads
-		if(numPolyVerts == 4)
+		if (numPolyVerts == 4)
 		{
 			batch->indices.append(faceIndices[0]);
 			batch->indices.append(faceIndices[1]);
@@ -361,14 +383,14 @@ void CRenderModel::GenerateBuffers()
 	Array<int> indices;
 
 	// merge batches
-	for(usize i = 0; i < batches.size(); i++)
-	{	
+	for (usize i = 0; i < batches.size(); i++)
+	{
 		//int startVertex = vertices.numElem();
 		int startIndex = indices.size();
-		
+
 		//vertices.append(batches[i]->vertices);
 
-		for(usize j = 0; j < batches[i]->indices.size(); j++)
+		for (usize j = 0; j < batches[i]->indices.size(); j++)
 			indices.append(batches[i]->indices[j]);
 
 		modelBatch_t batch;
@@ -377,13 +399,17 @@ void CRenderModel::GenerateBuffers()
 		batch.tpage = batches[i]->tpage;
 
 		m_batches.append(batch);
-		
+
 		delete batches[i];
 	}
-	
+
+	// if has existing one - regenerate
+	if (m_vao)
+		GR_DestroyVAO(m_vao);
+
 	m_vao = GR_CreateVAO(vertices.size(), indices.size(), (GrVertex*)vertices, (int*)indices, 0);
 
-	if(!m_vao)
+	if (!m_vao)
 	{
 		MsgError("Cannot create Model VAO!\n");
 	}
