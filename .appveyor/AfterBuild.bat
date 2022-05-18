b2b17266886adfb59d2952c6dbcf3b57d11a433f
Ilya Shurumov <ishurumov@gmail.com>
2022-05-18 14:40:18 +0600

    - attempt to fix appveyor build

--- .appveyor/AfterBuild.bat
+++ .appveyor/AfterBuild.bat
@@ -2,24 +2,10 @@
 
 set config=Debug:Release
 for %%c in (%config::= %) do (
-    cd %project_folder%\DriverLevelTool\bin\%%c
-	copy %windows_sdl2_dir%\lib\x86\SDL2.dll SDL2.dll /Y
+    cd %project_folder%\bin\%%c
+    
+    copy %windows_sdl2_dir%\lib\x86\SDL2.dll SDL2.dll /Y
 
     xcopy /e /v %data_folder% .\ /Y
-    7z a "DriverLevelTool_%%c.zip" ".\*"
-	
-	cd %project_folder%\DriverSoundTool\bin\%%c
-
-    xcopy /e /v %data_folder% .\ /Y
-    7z a "DriverSoundTool_%%c.zip" ".\*"
-	
-	cd %project_folder%\DriverImageTool\bin\%%c
-
-    xcopy /e /v %data_folder% .\ /Y
-    7z a "DriverImageTool_%%c.zip" ".\*"
-	
-	cd %project_folder%\Driver2CutsceneTool\bin\%%c
-
-    xcopy /e /v %data_folder% .\ /Y
-    7z a "Driver2CutsceneTool_%%c.zip" ".\*"
+    7z a "OpenDriverTools_%%c.zip" ".\*"
 )
--- .appveyor/Build.bat
+++ .appveyor/Build.bat
@@ -6,6 +6,8 @@ set SDL2_DIR=%windows_sdl2_dir%
 
 premake5 vs2019
 
+cd project_vs2019_windows
+
 set config=Debug:Release
 for %%c in (%config::= %) do (
     msbuild .\OpenDriver2Tools.sln /p:Configuration="%%c" /p:Platform=Win32 /m ^
